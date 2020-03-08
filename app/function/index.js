bff611b893bfece4cfceeeb96dbc46b356d599ea
NIKHIL JAIN <34137170+nikhiljainjain@users.noreply.github.com>
2020-03-08 17:10:22 +0530

    function file update

--- app/function/index.js
+++ app/function/index.js
@@ -2,24 +2,24 @@ let User = require("../model/user");
 let cookie = require("cookie");
 
 const studentValid = (req, res, next) =>{
-//	req.data = {
-//		"name": "Asann",
-//        "gender": "male",
-//        "contact": 9090909090,
-//        "regno": "17BCI0012",
-//        "parnetName": "Asann",
-//        "password": null,
-//        "restItem": [],
-//        "dailyBud": 100,
-//        "monthBud": 5000,
-//        "age": 10,
-//        "_id": "5e64b692153a78259089fd76",
-//        "loginTime": "2020-03-08T09:10:42.445Z",
-//        "__v": 0
-//	};
-//	next();
+	req.data = {
+		"name": "Asann",
+        "gender": "male",
+        "contact": 9090909090,
+        "regno": "17BCI0012",
+        "parnetName": "Asann",
+        "password": null,
+        "restItem": [],
+        "dailyBud": 100,
+        "monthBud": 5000,
+        "age": 10,
+        "_id": "5e64b692153a78259089fd76",
+        "loginTime": "2020-03-08T09:10:42.445Z",
+        "__v": 0
+	};
+	next();
 	
-    let regno = cookie.parse(req.headers.cookie || '');
+    /*let regno = cookie.parse(req.headers.cookie || '') || "";
 	if (regno != null){
 		regno = regno.regno || "17BCE0012";
 		User.findOne({regno}, (err, data)=>{
@@ -33,25 +33,42 @@ const studentValid = (req, res, next) =>{
 		});	
 	}else{
 		res.status(302).redirect("/student/login");
-	}
+	}*/
 };
 
 const parentValid = (req, res, next) =>{
-    let regno = cookie.parse(req.headers.cookie || '');
-	if (regno != null){
-		regno = regno.regno;
-		User.findOne({regno}, (err, data)=>{
-			if (err) throw console.error.bind(err);
-			if (data){
-				//req.data = data;
-				next();
-			}else{
-				res.status(302).redirect("/parent/login");		  
-			}
-		});	
-	}else{
-		res.status(302).redirect("/parent/login");
-	}
+	req.data = {
+		"name": "Asann",
+        "gender": "male",
+        "contact": 9090909090,
+        "regno": "17BCI0012",
+        "parnetName": "Asann",
+        "password": null,
+        "restItem": [],
+        "dailyBud": 100,
+        "monthBud": 5000,
+        "age": 10,
+        "_id": "5e64b692153a78259089fd76",
+        "loginTime": "2020-03-08T09:10:42.445Z",
+        "__v": 0
+	};
+	next();
+	
+//    let regno = cookie.parse(req.headers.cookie || '');
+//	if (regno != null){
+//		regno = regno.regno;
+//		User.findOne({regno}, (err, data)=>{
+//			if (err) throw console.error.bind(err);
+//			if (data){
+//				//req.data = data;
+//				next();
+//			}else{
+//				res.status(302).redirect("/parent/login");		  
+//			}
+//		});	
+//	}else{
+//		res.status(302).redirect("/parent/login");
+//	}
 };
 
 module.exports = { studentValid, parentValid };
