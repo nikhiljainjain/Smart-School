96a215b0c0a721b3f82013eeb28020656089e6a3
NIKHIL JAIN <34137170+nikhiljainjain@users.noreply.github.com>
2020-03-08 15:11:09 +0530

    cors value change

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('view engine', 'ejs');
 app.disable('etag');
 app.disable('x-powered-by');
 
-app.use(cors());
+app.use(Cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
